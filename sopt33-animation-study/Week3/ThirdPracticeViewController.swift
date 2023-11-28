2a54889794409f38be5140da06e51ca533e8dcaf
SeoYeon Choi <102604192+choiseoo@users.noreply.github.com>
2023-11-29 01:40:34 +0900

    3주차 애니메이션 실습

--- sopt33-animation-study/Week3/ThirdPracticeViewController.swift
+++ sopt33-animation-study/Week3/ThirdPracticeViewController.swift
@@ -9,8 +9,36 @@ import UIKit
 
 final class ThirdPracticeViewController: UIViewController {
     
+    override func loadView() {
+        super.loadView()
+        let view = testView(frame: UIScreen.main.bounds)
+        view.setAnimateBackGround()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+class testView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func draw(_ rect: CGRect) {
+        self.backgroundColor = .white
+    }
+    
+    func setAnimateBackGround() {
+        let animation = CABasicAnimation(keyPath: "backgroundColor")
+        animation.duration = 4
+        animation.fromValue = UIColor.red.cgColor
+        animation.toValue = UIColor.blue.cgColor
+        animation.isRemovedOnCompletion = false
+        self.layer.add(animation, forKey: "backgroundAni")
     }
 }
